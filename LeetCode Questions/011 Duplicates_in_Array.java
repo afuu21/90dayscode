25ffdc74eb641f4de888892767c60b615e5ea11e
Mohammed Afnan Ahmed <55314442+afuu21@users.noreply.github.com>
2022-05-05 17:11:52 +0530

    Create 011 Duplicates_in_Array.java

---
+++ LeetCode Questions/011 Duplicates_in_Array.java
@@ -0,0 +1,14 @@
+class Solution {
+    public List<Integer> findDuplicates(int[] nums) {
+        List<Integer> list = new ArrayList<Integer>();
+        int index;
+        for(int i=0; i<nums.length; i++) {
+            index=Math.abs(nums[i])-1;
+            if(nums[index] < 0)
+                list.add(index+1);
+            nums[index]=-nums[index];
+        }
+        
+        return list;
+    }
+}
